00db29ebbbc464bceb7c15d8e49635bc7bbb9903
AxelDona <axel.dona@outlook.com>
2022-02-21 23:16:18 +0100

    travail exo 4 & 6

--- TP1/exo3.cpp
+++ TP1/exo3.cpp
@@ -7,7 +7,7 @@ int search(int value, int array[], int size);
 
 int main(){
     int array[10] = {5, 14, 13, 9, 2, 3, 89, 12, 0, 56};
-    cout << search(5, array, 10) << endl;
+    cout << search(13, array, 10) << endl;
 }
 
 int search(int value, int array[], int size){
--- TP1/exo4.cpp
+++ TP1/exo4.cpp
@@ -3,18 +3,28 @@
 
 using namespace std;
 
-void allEvens(int *evens[], int array[], int evenSize, int arraySize);
+int allEvens(int evens[], int array[], int evenSize, int arraySize);
 
 int main(){
-    int array[8] = {12, 13, 14, 15, 16, 17, 18, 19};
-    int evens[];
-    allEvens(evens, array, 0, 8);
+    int array[7] ={ 4, 9, 17, 22, 100, 1, 23};
+    int evens[7] = {};
+    allEvens(evens, array, 0, 7);
+    for (int i=0; i<7; i++) {
+        cout << evens[i] << endl;
+    }
+
 }
 
-void allEvens(int evens[], int array[], int evenSize, int arraySize){
-    if(arraySize == 0){
-
+int allEvens(int evens[], int array[], int evenSize, int arraySize){
+    if (arraySize == 0) {
+        return -1;
     }
+    if (array[arraySize-1]%2 == 0) {
+        evens[evenSize] = array[arraySize-1];
+        return allEvens(evens, array, evenSize+1, arraySize-1);
+    }
+    return allEvens(evens, array, evenSize, arraySize-1);
+
 }
 
 

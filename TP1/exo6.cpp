31d58a7502f1ea24e6411ee53106f827e222e9ac
AxelDona <axel.dona@outlook.com>
2022-02-21 23:16:43 +0100

    travail exo 4 & 6

--- TP1/exo6.cpp
+++ TP1/exo6.cpp
@@ -9,8 +9,20 @@ struct DynaTableau{
     int* tab;
 };
 
-void ajoute(int val, DynaTableau* dyn);
-void affiche(DynaTableau dyn);
+struct Liste {
+    int val;
+    Liste *nxt;
+} ;
+
+void ajouteDyn(int val, DynaTableau* dyn);
+void afficheDyn(DynaTableau dyn);
+int recupereDyn(int n, DynaTableau dyn);
+int chercheDyn(int n, DynaTableau dyn);
+void stockeDyn(int n, int valeur, DynaTableau *dyn);
+void ajouteLst(int val, Liste* lis);
+int recupereLst(int n, Liste lis);
+int chercheLst(int n, Liste* lis);
+void stockeLst(int n, int newValeur, Liste *lis);
 
 int main(){
     DynaTableau leDyn;
@@ -19,20 +31,34 @@ int main(){
     int tableau[leDyn.tailleMax] = {12, 4, 5, 84};
     leDyn.tab = tableau;
     cout << endl;
-    affiche(leDyn);
-    ajoute(8, &leDyn);
+    cout << "-- DYNATABLEAUX-- " << endl << endl;
+    afficheDyn(leDyn);
+    ajouteDyn(8, &leDyn);
     cout << endl;
     cout << "On ajoute une valeur (8)" << endl;
     cout << endl;
-    affiche(leDyn);
-    ajoute(14, &leDyn);
+    afficheDyn(leDyn);
+    ajouteDyn(14, &leDyn);
     cout << endl;
     cout << "On ajoute encore une valeur (14)" << endl;
     cout << endl;
-    affiche(leDyn);
+    afficheDyn(leDyn);
+    cout << endl;
+    cout << "On récupère la 5ème valeur du tableau :" << endl;
+    cout << recupereDyn(4, leDyn) << endl;
+    cout << endl;
+    cout << "On cherche l'index de la valeur 84 dans le tableau :" << endl;
+    cout << chercheDyn(84, leDyn) << endl;
+    cout << endl;
+    cout << "On redéfinit en 99 la valeur de l'index 2 du tableau :" << endl;
+    stockeDyn(2, 99, &leDyn);
+    afficheDyn(leDyn);
+    cout << endl;
 }
 
-void ajoute(int val, DynaTableau* dyn){
+// FONCTIONS DYNATABLEAUX
+
+void ajouteDyn(int val, DynaTableau* dyn){
     if(dyn->taille < dyn->tailleMax){
         dyn->tab[dyn->taille] = val;
         dyn->taille +=1;
@@ -48,7 +74,7 @@ void ajoute(int val, DynaTableau* dyn){
     }
 }
 
-void affiche(DynaTableau dyn){
+void afficheDyn(DynaTableau dyn){
     cout << "Taille max : " << dyn.tailleMax << endl;
     cout << "Taille : " << dyn.taille << endl;
     for(int i = 0; i<dyn.taille; i++){
@@ -56,3 +82,61 @@ void affiche(DynaTableau dyn){
     }
     cout << endl;
 }
+
+int recupereDyn(int n, DynaTableau dyn){
+    return (dyn.tab[n]);
+}
+
+int chercheDyn(int n, DynaTableau dyn){
+    for (int i=0; i<dyn.taille; i++) {
+        if (dyn.tab[i] == n) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void stockeDyn(int n, int newValeur, DynaTableau *dyn){
+    dyn->tab[n] = newValeur;
+}
+
+// FONCTIONS LISTE
+
+void ajouteLst(int val, Liste* lis){
+    Liste* add = new Liste;
+    add->val = val;
+    lis->nxt = add;
+}
+
+int recupereLst(int n, Liste lis){
+    if (n == 0){
+        return lis.val;
+    }
+    else {
+        return (recupereLst(n-1, *(lis.nxt)));
+    }
+}
+
+int chercheLst(int n, Liste* lis){
+    int index = 0;
+    while (lis != nullptr){
+        if ((lis->val)==n){
+            return index;
+        }
+        index++;
+        lis = lis->nxt;
+    }
+    return -1;
+}
+
+void stockeLst(int n, int newValeur, Liste *lis){
+    int index = 0;
+    while (lis != nullptr){
+        if (index == n){
+            lis->val = newValeur;
+        }
+        index++;
+        lis = lis->nxt;
+    }
+}
+

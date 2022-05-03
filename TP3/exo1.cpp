5dcd8f2ece4f601dbdd6aa024e1e5ce22bc0e9ae
AxelDona <axel.dona@outlook.com>
2022-05-03 16:54:14 +0200

    Fin TP3

--- TP3/exo1.cpp
+++ TP3/exo1.cpp
@@ -1,21 +1,44 @@
-#include "tp3.h"
-#include <QApplication>
-#include <time.h>
+#include <iostream>
 
-MainWindow* w = nullptr;
-using std::size_t;
+using namespace std;
 
-int binarySearch(Array& array, int toSearch)
-{
-	return -1;
+int binarySearch(int tab[], int toSearch, int start, int end);
+void displayTab(int tab[], int size);
+
+int main(){
+    int size = 11;
+    int tab[size] = {2, 6, 7, 9, 15, 22, 51, 53, 57, 68, 75};
+    int start = 0;
+    int end = size;
+    int toSearch;
+    cout << displayTab(tab, size) << endl;
+    cout << "Entrez le nombre à rechercher dans le tableau :" << endl;
+    cin >> toSearch;
+    binarySearch(tab, toSearch, start, end);
 }
 
-int main(int argc, char *argv[])
-{
-	QApplication a(argc, argv);
-	MainWindow::instruction_duration = 500;
-	w = new BinarySearchWindow(binarySearch);
-	w->show();
+int binarySearch(int tab[], int toSearch, int start, int end){
+    while(start < end){
+        int mid = (start+end)/2;
 
-	return a.exec();
+        if (toSearch === mid){
+            int foundIndex = mid;
+            return foundIndex;
+        } else if (toSearch > tab[mid]){
+            start = mid+1;
+            binarySearch(tab, toSearch, start, end);
+        } else if (toSearch < tab[mid]){
+            end = mid;
+            binarySearch(tab, toSearch, start, end);
+        }
+    }
+    return -1;
+}
+
+void displayTab(int tab[], int size){
+    cout << "Tableau : ";
+    for(int i=0; i<size; i++){
+        cout << tab[i] << ", ";
+    }
+    cout << endl;
 }
